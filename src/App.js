1938df2368acdcaddac0231b05b768ac3edc85bd
Talilotu <nesikaufusi@gmail.com>
2022-07-06 00:19:25 +1000

    Added Homepage component and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Homepage from "./Homepage";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
