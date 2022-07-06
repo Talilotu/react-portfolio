2fa86eb444addb3b9efd5aef2c67b89e0e4ee509
Talilotu <nesikaufusi@gmail.com>
2022-07-07 00:19:40 +1000

    updated card background

--- src/components/Portfolio/Homepage.js
+++ src/components/Portfolio/Homepage.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Card from "../UI/Card";
+import Transition from "./Transition";
 
 import "./Homepage.css";
 
 const Homepage = () => {
   return (
     <Card>
-      <section>
-        <p>👋🏼 Hello, I am</p>
-        <h1>I am Akanesi Tohotoa</h1>
-      </section>
+      <card className="top-section">
+        <div className="row justify-content-start">
+          <div className="col-4">
+            <p>👋🏼 Hi, I am</p>
+            <h1>Akanesi Tohotoa</h1>
+            <Transition />
+          </div>
+          <div className="col-4">One of two columns</div>
+        </div>
+      </card>
     </Card>
   );
 };
