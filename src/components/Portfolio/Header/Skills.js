7d56f3e82b9a1a45185dbef4e5f6f084a0022355
Talilotu <nesikaufusi@gmail.com>
2022-07-12 02:57:40 +1000

    Added state to skills component

--- src/components/Portfolio/Header/Skills.js
+++ src/components/Portfolio/Header/Skills.js
@@ -1,24 +1,52 @@
-import React, { Fragment } from "react";
-import Button from "../../UI/Button";
+import React, { Fragment, useState } from "react";
 
 import "./Header.css";
 
 const Skills = (props) => {
+  const [frontEndList, setFrontEndList] = useState(true);
+  const [reactList, setReactList] = useState(false);
+
+  const FrontEndSkillsHandler = (event) => {
+    event.preventDefault();
+    setFrontEndList(true);
+    setReactList(false);
+  };
+
+  const ReactSkillsHandler = (event) => {
+    event.preventDefault();
+    setReactList(true);
+    setFrontEndList(false);
+  };
+
   return (
     <Fragment>
-      <div className="skills-button">
-        <Button className="btn">Front-end Skills</Button>
-        {""} <Button className="btn">React Skills</Button>
+      <div className="skills">
+        <button className="btn" type="button" onClick={FrontEndSkillsHandler}>
+          Front-end Skills +
+        </button>
+        {""}{" "}
+        <button className="btn" type="button" onClick={ReactSkillsHandler}>
+          React Skills +
+        </button>
       </div>
       <div className="front-end">
-        <h3>Front-end skills</h3>
-        <ul>{props.frontEndSkills}</ul>
+        {frontEndList && (
+          <div>
+            <h3>Front-end skills</h3>
+            <ul>{props.frontEndSkills}</ul>
+          </div>
+        )}
       </div>
+
       <div className="react-skills">
-        <h3>React skills</h3>
-        <ul>{props.reactSkills}</ul>
-        <p>React Hooks:</p>
-        <ul>{props.reactHooks}</ul>
+        {reactList && (
+          <div>
+            <h3>React skills</h3>
+            <ul>{props.reactSkills}</ul>
+            <p>React Hooks:</p>
+            <ul>{props.reactHooks}</ul>
+          </div>
+        )}
       </div>
     </Fragment>
   );
