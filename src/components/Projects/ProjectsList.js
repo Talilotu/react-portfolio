8cd64e38da9a9353db8b621fdd3b7ec1be5b32aa
Talilotu <nesikaufusi@gmail.com>
2022-07-12 17:20:52 +1000

    Added projects list

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -1,4 +1,18 @@
 import React from "react";
 
-const Projects = () => {};
+import "./Projects.css";
+
+const Projects = (props) => {
+  return (
+    <li className="list">
+      <div>
+        <h3>{props.name}</h3>
+        <div>{props.photo}</div>
+        <div>{props.url}</div>
+        <div>{props.description}</div>
+      </div>
+    </li>
+  );
+};
+
 export default Projects;
