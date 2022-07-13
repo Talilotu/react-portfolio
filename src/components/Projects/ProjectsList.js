9c3aa120de4ab03a01ee576d73e4a749083879ff
Talilotu <nesikaufusi@gmail.com>
2022-07-14 01:45:30 +1000

    added open source button

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -6,15 +6,20 @@ const Projects = (props) => {
   return (
     <li
       className="card projects-list"
-      style={{ width: "25rem", height: "32rem" }}
+      style={{ width: "25rem", height: "34rem" }}
     >
       <div className="card-img-top">{props.image}</div>
       <div className="card-body">
         <h2 className="card-title">{props.name}</h2>
         <p className="card-text">{props.description}</p>
-        <a href={props.url} className="btn" target="_blank" rel="noreferrer">
-          See project
-        </a>
+        <span className="url">
+          <a href={props.url} className="btn" target="_blank" rel="noreferrer">
+            See project
+          </a>
+          <a href={props.url} className="btn" target="_blank" rel="noreferrer">
+            open source
+          </a>
+        </span>
       </div>
     </li>
   );
