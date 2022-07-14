203c9e65b26e63ff4086582c69debb46267785c7
Talilotu <nesikaufusi@gmail.com>
2022-07-15 04:18:34 +1000

    add useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/UI/Navbar";
@@ -10,31 +10,39 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Header />
-      <Projects />
-      <main>
-        <Switch>
-          <Route path="/home">
-            <Header />
-            <Projects />
-          </Route>
-          <Route path="/training">
-            <Training />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </main>
-      <Footer />
-    </div>
-  );
+  const [loaded, setLoaded] = useState(true);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, [loaded]);
+
+  if (loaded) {
+    return (
+      <div className="App">
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/home">
+              <Header />
+              <Projects />
+            </Route>
+            <Route path="/training" exact>
+              <Training />
+            </Route>
+            <Route path="/projects" exact>
+              <Projects />
+            </Route>
+            <Route path="/contact" exact>
+              <Contact />
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default App;
